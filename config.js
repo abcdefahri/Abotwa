930eac781f199dc9b56fa6d25c96bb3b474f952c
abcdefahri <113069902+abcdefahri@users.noreply.github.com>
2022-10-13 05:00:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.owner = ['6288287764273']
+global.premium = ['6288287764273']
+global.packname = '-'
+global.author = 'Fahri WhatsApp Bot'
+global.sessionName = 'Fahri'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
